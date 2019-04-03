7ed456c56bad7ab312c6db9f8c08a479ba844694
Kaan Caliskan <mail@kcaliskan.com>
2019-04-03 00:18:14 -0700

    firebase deploy

---
+++ dist/precache-manifest.50b25e05b1d0eb08398a9541fe03e8d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b775c71a301617ab6b2f",
+    "url": "/static/css/main.e1cd44c4.chunk.css"
+  },
+  {
+    "revision": "b775c71a301617ab6b2f",
+    "url": "/static/js/main.234b0064.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "821b7dc3e33c34087cf1",
+    "url": "/static/js/2.83f6a84e.chunk.js"
+  },
+  {
+    "revision": "ec76f876c073c4b1bbc52829bc66adae",
+    "url": "/index.html"
+  }
+];
